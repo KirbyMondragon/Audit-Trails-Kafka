fe6f3298e85b2e84f011ff928a950ffe639d12a3
KevinMondragonDev <kevinmondragondev@gmail.com>
2026-01-28 22:07:18 -0600

    First commit

---
+++ src/main/java/com/springboot/kafkaaudit/KafkaAuditApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kafkaaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAuditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAuditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/kafkaaudit/KafkaAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.kafkaaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
